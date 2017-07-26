a127b0cc3a7f19819c812909d26fa996314d2542
Alyssa Nguyen <halongpbn@gmail.com>
2017-07-26 07:34:20 -0700

    Update home.js

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: uluru
+          center: uluru,
           styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
